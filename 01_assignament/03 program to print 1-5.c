224cbb3f5f364091c2eef35362dc95af466ec32f
Shubhamgharge <159037288+Shubhamgharge@users.noreply.github.com>
2024-10-24 21:32:24 +0530

    Add files via upload

---
+++ 01_assignament/03 program to print 1-5.c
@@ -0,0 +1,20 @@
+//03 program to print 1-5 on screen
+#include<stdio.h>
+void Display()
+{
+   int i=0;
+   
+    for(i=1; i<=5 ; )
+    {
+       printf("\n %d",i);
+       i++;
+    }
+ 
+}
+int main()
+{
+    Display();
+
+    return 0;
+
+}
