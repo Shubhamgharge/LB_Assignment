7bccccadc9579fee35b59409dd8262db0bf54525
Shubhamgharge <159037288+Shubhamgharge@users.noreply.github.com>
2024-10-24 21:31:57 +0530

    Add files via upload

---
+++ 01_assignament/02 program to print 5 times on screen.c
@@ -0,0 +1,20 @@
+//02 program to print 5 times marvellous on screen
+#include<stdio.h>
+void Display()
+{
+    int i=0;
+    
+    for(i=1 ; i<=5 ;i++)
+    {
+       printf("\n Marvellous");
+    
+    }
+ 
+}
+int main()
+{
+    Display();
+
+    return 0;
+
+}
