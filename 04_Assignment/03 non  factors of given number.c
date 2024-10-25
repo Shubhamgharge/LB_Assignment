d9adca03504430b61ba18f4620b282e3f5daf1c7
Shubhamgharge <159037288+Shubhamgharge@users.noreply.github.com>
2024-10-25 15:20:27 +0530

    Add files via upload

---
+++ 04_Assignment/03 non  factors of given number.c
@@ -0,0 +1,40 @@
+//03 non  factors of given number
+#include<stdio.h>
+#include<conio.h>
+
+int Multfactor(int);
+int Multfactor(int iNo)
+{
+   int i=0;
+   int Multi=1;
+   if(iNo <= 0 )
+   {
+      //return;
+      iNo=-iNo;
+   }
+     for(i = 1; i < iNo ;i++)
+     {
+       if(iNo % i !=0) 
+       {
+          printf("%d\t",i);
+         
+       }
+       
+     }
+     
+}
+int main()
+{
+    int iValue=0,iRes=0;
+    
+    
+    printf("\n Enter a Number : ");
+    scanf("%d",&iValue);
+ 
+    Multfactor(iValue);
+    
+    
+    getch();
+    return 0;
+
+}
