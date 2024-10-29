8c1eb3efcd1d3debd204e7d4adf5b7558325d7ce
Shubhamgharge <159037288+Shubhamgharge@users.noreply.github.com>
2024-10-29 21:43:09 +0530

    Add files via upload

---
+++ 11_Assignment/05 frequency of such a digit which are less then 6.c
@@ -0,0 +1,40 @@
+//05 frequency of such a digit which are less then 6
+#include<stdio.h>
+#include<conio.h>
+
+int Frequency(int);
+int Frequency(int iNo)
+{ 
+   int iDigit=0, iTemp=0, iCount=0;
+   
+   iTemp=iNo;
+   
+   while(iTemp > 0)
+   {
+       iDigit = iTemp % 10;
+       
+       if(iDigit < 6)
+       {
+           iCount++;
+       
+       }
+       iTemp = iTemp / 10;
+   }
+   return iCount;
+
+}
+int main()
+{ 
+    int iValue=0, iRet=0;
+    
+    printf("\n Enter number : ");
+    scanf("%d",&iValue);
+    
+    iRet=Frequency(iValue);
+    
+    printf("\n Frequency of 2 in %d is %d.",iValue,iRet);
+
+   getch();
+   return 0;
+
+}
