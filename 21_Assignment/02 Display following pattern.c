25587561feafd38d0e9a2e80be0032ac304931ad
Shubhamgharge <159037288+Shubhamgharge@users.noreply.github.com>
2024-11-01 21:38:08 +0530

    Add files via upload

---
+++ 21_Assignment/02 Display following pattern.c
@@ -0,0 +1,56 @@
+
+//////////////////////////////////////////////////////////////
+//                                                          //
+//   02 Accept no of row and column for below pattarn       //
+//                                                          //  
+//   input : iRow and iColumn : 4                           //
+//                                                          //
+//   output :  *    *    *    *                             //                               
+//             *    *    *    #                             //
+//             *    *    #    #                            //
+//             *    #    #    #                            //
+//                                                        //
+//                                                        //
+////////////////////////////////////////////////////////////
+
+#include<stdio.h>
+#include<conio.h>
+
+void pattern(int);
+void pattern(int iSize)
+{
+     int i=0, j=0;
+     
+     for( i=1 ; i<=iSize ; i++)
+     {
+         for( j=1 ; j<=iSize ; j++)
+         {
+            if(i+j<=iSize+1)
+            {
+              printf("  *  ");
+            }  
+            else
+            {
+               printf("  #  ");
+            }
+         }   
+         printf("\n");
+     }
+
+}
+int main()
+{ 
+    int iValue=0;
+    
+    printf("\niRow and iColumn : ");
+    scanf("%d",&iValue);
+    
+    
+    printf("\n");
+    
+    pattern(iValue);
+    
+    getch();
+    return 0;
+}
+    
