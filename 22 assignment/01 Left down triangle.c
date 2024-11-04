8493f789f5969b35a0125dd55c72d94ff251cdee
Shubhamgharge <159037288+Shubhamgharge@users.noreply.github.com>
2024-11-04 22:49:17 +0530

    Add files via upload

---
+++ 22 assignment/01 Left down triangle.c
@@ -0,0 +1,38 @@
+//01 Left down triangle
+
+#include<stdio.h>
+#include<conio.h>
+void pattarn(int);
+void pattarn(int iSize)
+{
+   int i=0, j=0;
+   for(i=1 ; i<=iSize ; i++)
+   {
+      for(j=1 ; j<=iSize ; j++ )
+      { 
+         if(i>=j) 
+         {
+           printf(" * ");
+         }
+         else
+         {
+           printf("   ");
+         }
+      }
+      printf("\n");
+   }
+
+
+}
+int main()
+{
+     int iValue=0;
+     
+     printf("\n Enter Row and Column size : ");
+     scanf("%d",&iValue);
+     
+     pattarn(iValue);
+     
+     getch();
+     return 0;
+}
