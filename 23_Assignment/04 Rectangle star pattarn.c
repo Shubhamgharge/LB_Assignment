fa332cfc009642f272718c2ead6b5b40932ba284
Shubhamgharge <159037288+Shubhamgharge@users.noreply.github.com>
2024-11-06 22:32:23 +0530

    Add files via upload

---
+++ 23_Assignment/04 Rectangle star pattarn.c
@@ -0,0 +1,43 @@
+//04 Rectangle star pattarn
+
+#include<stdio.h>
+#include<conio.h>
+
+void pattarn(int);
+void pattarn(int iSize)
+{
+   int i=0, j=0;
+   
+   for(i=1 ; i<=iSize ; i++)
+   {
+      for(j=1 ; j<=iSize ; j++ )
+      { 
+         if(i+j==iSize+1 || i==1 || j==1 || i==iSize || j==iSize) 
+         {
+           printf("  *  ");
+         }
+         else if(i+j>iSize)
+         { 
+            printf("  #  ");
+         }
+         else
+         {
+           printf("  $  ");
+         }
+      }
+      printf("\n");
+   }
+
+}
+int main()
+{
+     int iValue=0;
+     
+     printf("\n Enter Row and Column size : ");
+     scanf("%d",&iValue);
+     
+     pattarn(iValue);
+     
+     getch();
+     return 0;
+}
